acb8f40c55e6867c4a49cc1cde8954371a639b3a
Joel <joel@magdalenamedio.net>
2020-01-06 18:59:46 -0500

    Se colocan metadatos en archivo del plugin

---
+++ wp-content/plugins/zenvatitle.php
@@ -0,0 +1,8 @@
+/*
+* Plugin Name: Zenva Happy Title
+* Plugin URI: http://www.Zenva.com
+* Description: Simple effect en title
+* Verion: 1.0
+* Author URI: http://www.zenva.com
+* License: GPL2
+*/
